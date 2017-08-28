b62b5cdd1ff1da62403e1a41c73ca3ca149d7fd8
JohnMorris4 <johnmorris@morrisje.com>
2017-08-28 14:01:16 -0400

    Starting java programming class

---
+++ Hello/src/Morrisje/com/Main.java
@@ -0,0 +1,19 @@
+package Morrisje.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //variables
+
+
+
+        System.out.println("Hello John");
+	    int myFirstNumber = 5;
+	    int mySecondNumber = 12;
+	    int myThirdNumber = 8;
+	    int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+	    System.out.println("The Total is: " +   myTotal);
+
+        // write your code here
+    }
+}
